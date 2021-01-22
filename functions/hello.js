d8d65db7019ee1518c3dff77b2e2fcbab3c8e828
TopTalentDev <toptal715@gmail.com>
2021-01-22 13:29:12 -0700

    test

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify({ msg: "hello world" }),
+        body: JSON.stringify({ msg: "hello world - 1/22/2020" }),
     };
 };
