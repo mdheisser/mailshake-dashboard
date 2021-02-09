7bd6aaec745f8fdf949d9d50a1954bea37936275
TopTalentDev <toptal715@gmail.com>
2021-02-09 08:34:52 -0700

    addToMailshake only if contacts available

--- test.js
+++ test.js
@@ -6,22 +6,22 @@ const AirtableApi = require("./functions/utils/airtable");
 (async () => {
     try {
         // const client = "Rooftek";
-        const client = "Summa Media";
-        await fetch(
-            "https://mailshake-dashboard-9dc4b3.netlify.live/.netlify/functions/clientMailshake",
-            {
-                method: "POST",
-                body: JSON.stringify(client),
-            }
-        );
+        // const client = "Summa Media";
+        // await fetch(
+        //     "https://mailshake-dashboard-9dc4b3.netlify.live/.netlify/functions/clientMailshake",
+        //     {
+        //         method: "POST",
+        //         body: JSON.stringify(client),
+        //     }
+        // );
         // ROOFTEK
         // const Mailshake = new MailShakeApi(api);
         // const campaigns = await Mailshake.listCampaigns();
         // console.log(campaigns);
         // ROOFTEK - AIRTABLE
-        // const Airtable = new AirtableApi(key);
-        // const campaign = await Airtable.getCampaign("app115xQzw1jhn5U8");
-        // console.log(campaign);
+        const Airtable = new AirtableApi("key2tZxeaXDfyBJ9k");
+        const airtableContacts = await Airtable.getContacts("app115xQzw1jhn5U8");
+        console.log(airtableContacts.length > 0);
         // await Airtable.updateCampaign("app115xQzw1jhn5U8", campaign.recordID);
     } catch (error) {
         console.log("ERROR FETCHING ---", error);
