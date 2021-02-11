874de0355e24a685aa96a0f47a792232cdb244a2
TopTalentDev <toptal715@gmail.com>
2021-02-11 13:21:29 -0700

    replace with gSheet env

--- functions/utils/googleSheets.js
+++ functions/utils/googleSheets.js
@@ -26,7 +26,8 @@ module.exports = class GoogleSpreadsheetApi {
             // Initialize Auth
             await doc.useServiceAccountAuth({
                 client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-                private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+                // private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+                private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
             });
 
             await doc.loadInfo(); // loads document properties and worksheets
