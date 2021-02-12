938f18786523cb1b92ee1fd52e6e7d52945e1523
TopTalentDev <toptal715@gmail.com>
2021-02-11 17:10:34 -0700

    testing env

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
+
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify({ msg: "hello world - 2/12/2020" }),
+        // body: JSON.stringify({ msg: "hello world - 2/12/2020" }),
+        body: JSON.stringify({ msg: process.env.GOOGLE_PRIVATE_KEY.split("\\n").join("\n") }),
     };
 };
