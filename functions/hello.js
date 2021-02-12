c024e06157d651aaadb0574390f656f2a5f4c01c
TopTalentDev <toptal715@gmail.com>
2021-02-11 17:25:34 -0700

    log env

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 
 exports.handler = async (event) => {
+    console.log("client_email =", process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL);
+    console.log("private_key =", process.env.GOOGLE_PRIVATE_KEY);
+    console.log("private_key =", process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"));
+
     return {
         statusCode: 200,
         body: JSON.stringify({ msg: "hello world - 2/12/2020" }),
