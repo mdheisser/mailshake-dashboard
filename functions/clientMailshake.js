cb60c04c91570c7afe194cf8bfd25584e8d94add
TopTalentDev <toptal715@gmail.com>
2021-04-12 10:04:08 -0600

    move clientOureach to gCloud

--- functions/clientMailshake.js
+++ functions/clientMailshake.js
@@ -3,12 +3,12 @@ const emailProgram = require("./utils/emailProgram");
 
 exports.handler = async (event) => {
     if (event.httpMethod === "GET") {
-        // return {
-        //     statusCode: 200,
-        //     body: JSON.stringify({ msg: "POST request only" }),
-        // };
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ msg: "POST request only" }),
+        };
 
-        return await emailProgram();
+        // return await emailProgram();
     } else if (event.httpMethod === "POST") {
         return await addToMailshake(event);
     } else {
