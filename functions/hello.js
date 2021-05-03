52852bfd1679eb0cc279a26d9c2c365fe3418915
TopTalentDev <toptal715@gmail.com>
2021-05-03 15:46:39 -0600

    test route

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,20 @@
 exports.handler = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ msg: "hello world - 2/12/2020" }),
-    };
+    if (event.httpMethod === "GET") {
+        console.log(event);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ msg: event }),
+        };
+    } else if (event.httpMethod === "POST") {
+        console.log(event);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ msg: event }),
+        };
+    } else {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ msg: "Error" }),
+        };
+    }
 };
