f0a9bc3fab33b097c599e206f1785be4a1b05dc0
TopTalentDev <toptal715@gmail.com>
2021-07-26 10:25:18 -0600

    email response

---
+++ functions/emailResponse.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const axios = require("axios");
+
+exports.handler = async (event) => {
+    if (event.httpMethod === "GET") {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ msg: "POST request only" }),
+        };
+    } else if (event.httpMethod === "POST") {
+        const res = JSON.parse(event.body);
+        console.log(`\n${res}\n`);
+
+        await axios.post(process.env.SLACK_EMAIL_NOTIFICATIONS, {
+            text: "\n*Client:* Greenscape\n*Campaign:* Revive Lost Leads\n*Response:* Coming soon...\n",
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ msg: "POST request only" }),
+        };
+    } else {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ msg: "Error" }),
+        };
+    }
+};
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const axios = require("axios");
 const moment = require("moment");
 
 const fetch = require("node-fetch");
@@ -28,60 +29,9 @@ const { responseStatus } = require("./functions/utils/helpers");
 
 (async () => {
     try {
-        // const Mailshake = new MailShakeApi(foundUser.mailshakeApi);
-        // const listOfReplies = await Mailshake.getEmailAction(646229, "replied");
-        // const emailsReplied = listOfReplies.map((reply) => reply.emailAddress);
-        // console.log(emailsReplied);
-        // const client = "Rooftek";
-        // const client = "Summa Media";
-        // await fetch("https://mailshake-dashboard.netlify.app/.netlify/functions/clientMailshake", {
-        //     method: "POST",
-        //     body: JSON.stringify(client),
-        // });
-        //
-        // MAILSHAKE
-        // const Mailshake = new MailShakeApi(foundUser.mailshakeApi);
-        // const campaigns = await Mailshake.listCampaigns();
-        // console.log(campaigns);
-        // const listOfReplies = await Mailshake.getEmailAction(681215, "replied");
-        // const emailsReplied = listOfReplies.map((reply) => reply.emailAddress);
-        // console.log(listOfReplies);
-        // console.log(listOfReplies.length);
-        //
-        // GOOGLE SHEETS
-        // const GoogleSpreadsheet = new GoogleSpreadsheetApi(
-        //     "1JEGup18CLuqATWKPcJYF6jUBXWtQNBvSjxPBVZaPZLk"
-        // );
-        // await GoogleSpreadsheet.appendProspect(["Ryan Roman"]);
-        //
-        // GET CAMPAIGNS
-        // const getCampaigns = await Airtable.getCampaigns();
-        // let campaigns = liveCampaigns(getCampaigns);
-        // campaigns = campaignsDueToday(campaigns);
-        // campaigns = campaignsToRun(campaigns);
-        // console.log(campaigns);
-        // GET CONTACT BY
-        // const campaign = {
-        //     name: "Norman Mobile Numbers",
-        // };
-        // const full_name = "Kandi Malphrus";
-        // const message = {
-        //     response: "Can Ben give me a call today",
-        // };
-        //
-        // const now = moment().format("MM/DD/YYYY hh:mm a");
-        // const contact = await Airtable.findTextContact("appoNqmB15dMPPEXD", "Phil Reagan");
-        // await Airtable.updateContact("appoNqmB15dMPPEXD", contact.recordID, {
-        //     ["Response Date"]: new Date(),
-        // });
-
-        const getTextCampaigns = await Airtable.getCampaigns("Text");
-        const getCRMCampaigns = await Airtable.getCampaigns("CRM");
-        const getCampaigns = [...getTextCampaigns, ...getCRMCampaigns];
-        // const textCampaigns = getCampaigns.filter(
-        //     (foundCampaign) => foundCampaign.Campaign === campaign.name
-        // );
-        console.log(getCampaigns);
+        await axios.post(process.env.SLACK_EMAIL_NOTIFICATIONS, {
+            text: "\n*Client:* Greenscape\n*Campaign:* Revive Lost Leads\n*Response:* Coming soon...\n",
+        });
     } catch (error) {
         console.log("ERROR FETCHING ---", error);
     }
