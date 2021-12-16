b291713abb0785e284b70c122e3f1606a9638ce7
TopTalentDev <toptal715@gmail.com>
2021-12-16 14:00:16 -0700

    update

--- functions/emailResponse.js
+++ functions/emailResponse.js
@@ -19,7 +19,7 @@ exports.handler = async (event) => {
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ msg: "POST request only" }),
+            body: JSON.stringify({ res }),
         };
     } else {
         return {
